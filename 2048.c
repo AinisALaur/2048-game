afefaa4cdf266aae600da736025c2800a5fff554
Ainis Laurinavičius <ainis5002@gmail.com>
2024-11-09 16:24:40 +0200

    Prints value in the cell

--- 2048.c
+++ 2048.c
@@ -1,15 +1,22 @@
 #include<stdio.h>
 #define SQUARESIZE 11
 
-void DrawBoard(int *board[]){
+void DrawBoard(int board[4][4]){
     for(int t=0; t<4; ++t){ //how many total rows
         for(int h=0; h<SQUARESIZE/2; ++h){ //height of 1 row
             for(int r=0; r<4; ++r){ // How many rows
                 for(int i=0; i<SQUARESIZE; ++i){ // How many collums
-                    if(h==0 || h==SQUARESIZE/2-1 || i==0 || i==SQUARESIZE-1)
+                    //printf("%d", i);
+                    if (h==SQUARESIZE/4 && i==0)
+                        printf("#%9d#", 1, " ");
+
+                    if(h>0 && h<SQUARESIZE/2-1 && h!=SQUARESIZE/4 && i==0)
+                        printf("#%9s#", " ");
+
+
+                    if(h==0 || h==SQUARESIZE/2-1)
                         printf("#");
-                    else
-                        printf(" ");
+
                 }printf(" ");
             }printf("\n");
         }printf("\n");
@@ -21,6 +28,13 @@ void DrawBoard(int *board[]){
 int main(){
 
     int board[4][4] = {};
+    for(int i=0; i<4; ++i){
+        for(int x=0; x<4; ++x){
+            board[i][x] = 1;
+        }
+
+    }
+
     DrawBoard(board);
 
 
