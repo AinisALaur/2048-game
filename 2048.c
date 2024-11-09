b32b62ab83eb7a5018044c8bce77ddfb54ce0e7a
Ainis Laurinavičius <ainis5002@gmail.com>
2024-11-09 16:26:57 +0200

    Fixed DrawBoard() functions argument

--- 2048.c
+++ 2048.c
@@ -16,21 +16,21 @@ int GetNumSize(int value){
 }
 
 
-void DrawBoard(int board[4][4]){
+void DrawBoard(int *board){
     for(int t=0; t<4; ++t){ //how many total rows
         for(int h=0; h<SQUARESIZE/2; ++h){ //height of 1 row
             for(int r=0; r<4; ++r){ // How many rows
                 for(int i=0; i<SQUARESIZE; ++i){ // How many collums
-                    if (h==SQUARESIZE/4 && i==0){
-                        int sizeOfValue = GetNumSize(board[t][r]);
+                    if (h==SQUARESIZE/4 && i==0){ // Write number with correct alignment
+                        int sizeOfValue = GetNumSize(*(board+t+4*r));
                         if(sizeOfValue<=3)
-                            printf("#%s%5d%3s%s#", COLOR, board[t][r], " ", COLOR_RESET);
+                            printf("#%s%5d%3s%s#", COLOR, *(board+t+4*r), " ", COLOR_RESET);
                         else if(sizeOfValue<=5)
-                            printf("#%s%6d%2s%s#", COLOR,board[t][r], " ", COLOR_RESET);
+                            printf("#%s%6d%2s%s#", COLOR, *(board+t+4*r), " ", COLOR_RESET);
                         else if(sizeOfValue<=7)
-                            printf("#%s%7d%1s%s#", COLOR, board[t][r], " ", COLOR_RESET);
+                            printf("#%s%7d%1s%s#", COLOR, *(board+t+4*r), " ", COLOR_RESET);
                         else
-                            printf("#%s%8d#%s", COLOR, board[t][r], COLOR_RESET);
+                            printf("#%s%8d#%s", COLOR, *(board+t+4*r), COLOR_RESET);
                     }
                     if(h>0 && h<SQUARESIZE/2-1 && h!=SQUARESIZE/4 && i==0)
                         printf("#%8s#", " ");
@@ -44,6 +44,7 @@ void DrawBoard(int board[4][4]){
 
 
 
+
 int main(){
     srand(time(NULL));
 
@@ -54,7 +55,6 @@ int main(){
     int x, y;
 
     for(int i=0; i<2; ++i){
-
         int IsOccupied = 1;
         while(IsOccupied){
             x = rand()%5;
@@ -63,16 +63,12 @@ int main(){
             if(occupied[x][y] == 0)
                 IsOccupied = 0;
         }
-
         int newCellValue[10] = {4,2,2,2,2,2,2,2,2,2};
         int newIndex = rand()%11;
-
         occupied[x][y] = -1;
         board[x][y] = newCellValue[newIndex];
         ++occupiedCells;
-
     }
-
 
 
     DrawBoard(board);
